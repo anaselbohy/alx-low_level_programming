0fe3b0477a8842633b46e6447331c656d084cd13
anaselbohy <elbohyanas@gmail.com>
2023-08-09 22:21:48 -0700

    Anas

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-c program
+ * Description: C program
+ * Return: 0 (always)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
