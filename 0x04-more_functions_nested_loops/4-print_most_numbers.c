5348f806d03b52b00999a1d5b6c2440fff6bb2e1
anaselbohy <elbohyanas@gmail.com>
2023-08-17 02:57:50 -0700

    Anas

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,8 +12,8 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-	if (num != 4 && num != 2)
-	_putchar(num + '0');
+		if (num != 4 && num != 2)
+			_putchar(num + '0');
 	}
 	_putchar('\n');
 }
