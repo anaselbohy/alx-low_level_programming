94f665decef685590ee6dbd6c1c1e2227def53dd
anaselbohy <elbohyanas@gmail.com>
2023-08-21 04:23:20 -0700

    Anas

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,24 +5,24 @@
  *
  * @s: string to be converted
  *
- * Return: the int converted from the str
+ * Return: the int converted from the string
 */
 
 int _atoi(char *s)
 {
-	int i, d, n, length, f, digit;
+	int i, d, n, len, f, digit;
 
 	i = 0;
 	d = 0;
 	n = 0;
-	length = 0;
+	len = 0;
 	f = 0;
 	digit = 0;
 
-	while (s[length] != '\0')
+	while (s[len] != '\0')
 		len++;
 
-	while (i < length && f == 0)
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
